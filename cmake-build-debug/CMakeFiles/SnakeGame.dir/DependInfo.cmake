d91b18e36923931ed1c8fdc22aa96b90b4936ca1
Aitzha <gabit.aitzhan@gmail.com>
2021-04-05 15:44:52 +0600

    SnakeGame

---
+++ cmake-build-debug/CMakeFiles/SnakeGame.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/gabitajtzan/Desktop/SnakeGame/src/controller.cpp" "/Users/gabitajtzan/Desktop/SnakeGame/cmake-build-debug/CMakeFiles/SnakeGame.dir/src/controller.cpp.o"
+  "/Users/gabitajtzan/Desktop/SnakeGame/src/game.cpp" "/Users/gabitajtzan/Desktop/SnakeGame/cmake-build-debug/CMakeFiles/SnakeGame.dir/src/game.cpp.o"
+  "/Users/gabitajtzan/Desktop/SnakeGame/src/main.cpp" "/Users/gabitajtzan/Desktop/SnakeGame/cmake-build-debug/CMakeFiles/SnakeGame.dir/src/main.cpp.o"
+  "/Users/gabitajtzan/Desktop/SnakeGame/src/renderer.cpp" "/Users/gabitajtzan/Desktop/SnakeGame/cmake-build-debug/CMakeFiles/SnakeGame.dir/src/renderer.cpp.o"
+  "/Users/gabitajtzan/Desktop/SnakeGame/src/snake.cpp" "/Users/gabitajtzan/Desktop/SnakeGame/cmake-build-debug/CMakeFiles/SnakeGame.dir/src/snake.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/sdl2/2.0.14_1/include/SDL2"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
